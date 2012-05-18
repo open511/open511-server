b36ba461a54ff4a0b361b0499d5f6fe99ea14a5d
Michael Mulley <michael@michaelmulley.com>
2012-05-18 14:31:18 -0400

    Better handle invalid GML input

--- open511/utils/postgis.py
+++ open511/utils/postgis.py
@@ -1,10 +1,20 @@
-from django.db import connection
+from django.db import connection, transaction
+from django.db.utils import DatabaseError
 
+@transaction.commit_manually
 def gml_to_ewkt(gml_string, force_2D=False):
     cursor = connection.cursor()
     if force_2D:
         sql = 'SELECT ST_AsEWKT(ST_Force_2D(ST_GeomFromGML(%s)))'
     else:
         sql = 'SELECT ST_AsEWKT(ST_GeomFromGML(%s))'
-    cursor.execute(sql, [gml_string])
-    return cursor.fetchone()[0]
+    try:
+        cursor.execute(sql, [gml_string])
+        return cursor.fetchone()[0]
+    except DatabaseError as e:
+        transaction.rollback()
+        if 'invalid GML' in unicode(e):
+            raise ValueError("Invalid GML: %s" % gml_string)
+        raise
+    finally:
+        transaction.rollback()
