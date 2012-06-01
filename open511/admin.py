73e65fffe0c503f62d3657f16c050865b0502c3a
Michael Mulley <michael@michaelmulley.com>
2012-05-31 23:26:42 -0400

    Add a couple of admin options

--- open511/admin.py
+++ open511/admin.py
@@ -3,6 +3,7 @@ from django.contrib.gis import admin
 from open511.models import RoadEvent
 
 class RoadEventAdmin(admin.OSMGeoAdmin):
-    pass
+    list_display = ['source_id', 'title', 'jurisdiction', 'start_date', 'end_date']
+    list_filter = ['jurisdiction', 'start_date', 'end_date']
     
 admin.site.register(RoadEvent, RoadEventAdmin)
