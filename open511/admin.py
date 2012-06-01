719c6c9dbd4dd8583893f388b9aad9e9024eca8a
Michael Mulley <michael@michaelmulley.com>
2012-05-31 23:28:13 -0400

    Don't include source_id in admin list

--- open511/admin.py
+++ open511/admin.py
@@ -3,7 +3,7 @@ from django.contrib.gis import admin
 from open511.models import RoadEvent
 
 class RoadEventAdmin(admin.OSMGeoAdmin):
-    list_display = ['source_id', 'title', 'jurisdiction', 'start_date', 'end_date']
+    list_display = ['title', 'jurisdiction', 'start_date', 'end_date']
     list_filter = ['jurisdiction', 'start_date', 'end_date']
     
 admin.site.register(RoadEvent, RoadEventAdmin)
