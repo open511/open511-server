d3db7930efd20fd88ac94b5afc407c8b37e4e3ea
Michael Mulley <michael@michaelmulley.com>
2012-07-12 15:42:14 -0400

    Update MTQ scraper to work with serialization code changes

--- open511/scripts/scrape_mtq_to_open511.py
+++ open511/scripts/scrape_mtq_to_open511.py
@@ -14,9 +14,10 @@ import time
 from django.contrib.gis.geos import Point
 
 from lxml import etree
+from lxml.builder import E
 import lxml.html
 
-from open511.utils.serialization import roadevent_to_xml_element, get_base_open511_element
+from open511.utils.serialization import geom_to_xml_element, get_base_open511_element
 
 logger = logging.getLogger(__name__)
 
@@ -32,8 +33,6 @@ JURISDICTION = 'converted.quebec511.gouv.qc.ca'
 BASE_LIST_URL = 'http://carte.quebec511.gouv.qc.ca/fr/Element.ashx'
 BASE_DETAIL_URL = 'http://carte.quebec511.gouv.qc.ca/fr/Fenetres/FenetreTravailRoutier.aspx?id='
 
-class DummyRoadEvent(object):
-    pass
 
 def get_list_of_chantiers(action='EntraveMajeure', bounds=ALL_QUEBEC_BOUNDS):
     params = {
@@ -51,53 +50,66 @@ def get_list_of_chantiers(action='EntraveMajeure', bounds=ALL_QUEBEC_BOUNDS):
         logger.error("Could not load JSON from %s: %r" % (url, e))
         return []
 
+
 def get_roadevent_from_summary(summary):
 
-    rdev = DummyRoadEvent()
-    rdev.source_id = JURISDICTION + ':' + summary['id']
+    elem = E.RoadEvent(id=JURISDICTION + ':' + summary['id'])
 
-    #rdev.title = summary['info']
-    rdev.geom = Point(float(summary['lng']), float(summary['lat']), srid=4326)
+    elem.append(
+        E.Geometry(
+            geom_to_xml_element(
+                Point(float(summary['lng']), float(summary['lat']), srid=4326)
+            )
+        )
+    )
 
     url = BASE_DETAIL_URL + summary['id']
     resp = urllib2.urlopen(url)
-    time.sleep(1)
+    time.sleep(0.5)
+
+    def set_val(tag, val):
+        if val not in (None, ''):
+            e = etree.Element(tag)
+            e.text = unicode(val)
+            elem.append(e)
 
     root = lxml.html.fragment_fromstring(resp.read().decode('utf8'))
-    rdev.title = _get_text_from_elems(root.cssselect('#tdIdentification'))
-    rdev.description = _get_text_from_elems(root.cssselect('#tdDescriptionEntrave,#tdDetail'))
-    rdev.affected_roads = _get_text_from_elems(root.cssselect('#tdLocalisation'))
-    rdev.traffic_restrictions = _get_text_from_elems(root.cssselect('#tdRestrictionCamionnage'))
+    set_val('Title', _get_text_from_elems(root.cssselect('#tdIdentification')))
+    set_val('Description', _get_text_from_elems(root.cssselect('#tdDescriptionEntrave,#tdDetail')))
+    set_val('AffectedRoads', _get_text_from_elems(root.cssselect('#tdLocalisation')))
+    set_val('TrafficRestrictions', _get_text_from_elems(root.cssselect('#tdRestrictionCamionnage')))
 
     start_date = _get_text_from_elems(root.cssselect('#tdDebut'))
     end_date = _get_text_from_elems(root.cssselect('#tdFin'))
     if start_date:
-        rdev.start_date = _str_to_date(start_date)
+        set_val('StartDate', _str_to_date(start_date))
     if end_date:
-        rdev.end_date = _str_to_date(end_date)
+        set_val('EndDate', _str_to_date(end_date))
 
-    return rdev
+    return elem
+
 
 def main():
 
     logging.basicConfig()
 
-    base = get_base_open511_element()
+    base = get_base_open511_element(lang='fr')
 
     for summary in chain(
             get_list_of_chantiers(action='EntraveMajeure'),
             get_list_of_chantiers(action='EntraveMineure')):
         rdev = get_roadevent_from_summary(summary)
-        elem = roadevent_to_xml_element(rdev)
-        base.append(elem)
+        base.append(rdev)
 
     print etree.tostring(base, pretty_print=True)
+
 
 def _str_to_date(s):
     """2012-02-12 to a datetime.date object"""
     return datetime.date(*[
     int(x) for x in s.split('-')
     ])
+
 
 def _get_text_from_elem(elem, include_tail=False):
     # Same as elem.text_content(), but replaces <br> with linebreaks
@@ -107,6 +119,7 @@ def _get_text_from_elem(elem, include_tail=False):
         ''.join([_get_text_from_elem(e, True) for e in elem]),
         (elem.tail if elem.tail and include_tail else '')
     ])
+
 
 def _get_text_from_elems(elems):
     return '\n\n'.join(_get_text_from_elem(e) for e in elems)
