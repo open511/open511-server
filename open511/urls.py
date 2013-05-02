2ca1f220fc2fb69a5d115bf1239a860d532a30d0
Michael Mulley <michael@michaelmulley.com>
2013-05-02 13:01:54 -0400

    Periods in slug URLs

--- open511/urls.py
+++ open511/urls.py
@@ -3,12 +3,12 @@ from django.conf import settings
 
 urlpatterns = patterns('open511.views',
     url(r'^events/$', 'events.list_roadevents', name='open511_roadevent_list'),
-    url(r'^events/(?P<jurisdiction_slug>[a-z0-9-]+)/$', 'events.list_roadevents',
+    url(r'^events/(?P<jurisdiction_slug>[a-z0-9.-]+)/$', 'events.list_roadevents',
         name='open511_roadevent_list'),
-    url(r'^events/(?P<jurisdiction_slug>[a-z0-9-]+)/(?P<id>[^/]+)/$', 'events.roadevent',
+    url(r'^events/(?P<jurisdiction_slug>[a-z0-9.-]+)/(?P<id>[^/]+)/$', 'events.roadevent',
         name='open511_roadevent'),
     url(r'^jurisdictions/$', 'jurisdictions.list_jurisdictions'),
-    url(r'^jurisdictions/(?P<slug>[a-z0-9-]+)/$', 'jurisdictions.jurisdiction', name='open511_jurisdiction'),
+    url(r'^jurisdictions/(?P<slug>[a-z0-9.-]+)/$', 'jurisdictions.jurisdiction', name='open511_jurisdiction'),
     url(r'^$', 'discovery.discovery', name='open511_discovery'),
 )
 
