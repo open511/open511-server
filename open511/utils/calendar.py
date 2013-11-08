4ce4d32f2aa589d1cac69b7e6ff4269b1e74634d
Michael Mulley <michael@michaelmulley.com>
2013-11-08 01:00:33 -0500

    Fix aspect of specific_dates handling

--- open511/utils/calendar.py
+++ open511/utils/calendar.py
@@ -47,7 +47,7 @@ class Schedule(object):
         to the base recurrence pattern."""
         ex = {}
         for sd in self.root.xpath('schedule/specific_dates/specific_date'):
-            bits = sd.split(' ')
+            bits = unicode(sd.text).split(' ')
             date = text_to_date(bits.pop(0))
             ex.setdefault(date, []).extend([
                 _time_text_to_period(t)
