21a4b02fbd711db390542ba6e73ebc0554210c4d
Michael Mulley <michael@michaelmulley.com>
2014-03-22 14:23:43 -0400

    setuptools dependency in Travis

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages = find_packages(),
     dependency_links = [
         'https://www.djangoproject.com/download/1.7b1/tarball/#egg=Django-1.7b1',
-        'https://www.github.com/opennorth/open511/archive/master.zip#egg=open511',
+        'https://www.github.com/opennorth/open511/archive/master.zip#egg=open511-0.2',
     ],
     namespace_packages = ['open511'],
     install_requires = [
