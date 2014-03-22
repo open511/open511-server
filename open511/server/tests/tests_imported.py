46cd613bea4f5c2573132ed1c690128a50ff6a7c
Michael Mulley <michael@michaelmulley.com>
2014-03-22 13:41:25 -0400

    Update test package name

--- open511/server/tests/tests_imported.py
+++ open511/server/tests/tests_imported.py
@@ -7,15 +7,15 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 try:
-    from open511_api_tests.base import set_options as set_external_test_options
+    from open511.api_tests.base import set_options as set_external_test_options
     set_external_test_options(
         api_url=reverse('open511_discovery'),
         test_endpoint_url=None,
         use_django_test_client=True
     )
-    from open511_api_tests import *
+    from open511.api_tests import *
 except ImportError:
-    logging.warning("Could not import tests from the open511_api_tests package. Perhaps you need to install it?")
+    logging.warning("Could not import tests from the open511-api-tests package. Perhaps you need to install it?")
 
 
 # class MultilingualTest(TestCase):
