667208d4955d5b99d6ad4905b60c474d1457338a
Michael Mulley <michael@michaelmulley.com>
2014-11-19 14:56:35 -0500

    Add schedule migration

---
+++ open511_server/migrations/0006_schedule_data.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+from lxml import etree
+from lxml.builder import E
+
+def convert_schedule(orig_schedule):
+    # Takes an lxml Element for <schedules>, returns a new <schedule> Element
+    nu = etree.Element('schedule')
+    recurring = etree.Element('recurring_schedules')
+    specific = []
+    for child in orig_schedule:
+        if child.xpath('specific_dates'):
+            specific = child.xpath('specific_dates/specific_date/text()')
+        else:
+            child.tag = 'recurring_schedule'
+            recurring.append(child)
+    if len(recurring):
+        nu.append(recurring)
+        if specific:
+            nu.append(E.exceptions(*[E.exception(s) for s in specific]))
+    elif specific:
+        # We have exceptions but no recurring schedules.
+        intervals = E.intervals()
+        for s in specific:
+            date, _, times = s.partition(' ')
+            for timepair in times.split(' '):
+                start_time, end_time = timepair.split('-')
+                intervals.append(E.interval('{}T{}/{}T{}'.format(date, start_time, date, end_time)))
+        assert len(intervals)
+        nu.append(intervals)
+    else:
+        raise ValueError("Invalid schedule data: %s" % etree.tostring(orig_schedule))
+    return nu
+
+def update_schedule_data(apps, schema_editor):
+    RoadEvent = apps.get_model("open511", "RoadEvent")
+    for event in RoadEvent.objects.all():
+        elem = etree.fromstring(event.xml_data)
+        try:
+            old_sched = elem.xpath('schedules')[0]
+        except IndexError:
+            print etree.tostring(elem)
+            raise
+        new_sched = convert_schedule(old_sched)
+        elem.remove(old_sched)
+        elem.append(new_sched)
+        event.xml_data = etree.tostring(elem)
+        event.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('open511', '0005_roadevent'),
+    ]
+
+    operations = [
+        migrations.RunPython(update_schedule_data)
+    ]
