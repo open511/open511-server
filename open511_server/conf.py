ebee4e53b106f436c2608318c8b805e8e4251189
Michael Mulley <michael@michaelmulley.com>
2015-11-26 16:33:10 -0500

    Add option to disable all edit views

--- open511_server/conf.py
+++ open511_server/conf.py
@@ -7,6 +7,9 @@ class Open511Settings(AppConf):
 
     ENABLE_TEST_ENDPOINT = False
     DEFAULT_VERSION = 'v1'
+    # Even if ALLOW_EDITING is True, users need to be logged in
+    # via django auth, and explicitly given edit permissions on a jurisdiction
+    ALLOW_EDITING = True
 
     class Meta:
         prefix = 'OPEN511'
--- open511_server/utils/auth.py
+++ open511_server/utils/auth.py
@@ -1,6 +1,8 @@
+from open511_server.conf import settings
+
 def can(request, permission):
     """Is this request allowed the given permission?
 
     permission is a string label"""
     # Currently a dummy implementation that just delegates to django auth
-    return request.user.is_authenticated()
+    return settings.OPEN511_ALLOW_EDITING and request.user.is_authenticated()
